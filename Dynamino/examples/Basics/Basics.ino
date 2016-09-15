b8ad52945e658f77abb16eb7cae51744c732549a
Alistair <alistair.wk@gmail.com>
2016-09-15 17:18:02 +0100

    Minor changes

--- Dynamino/examples/Basics/Basics.ino
+++ Dynamino/examples/Basics/Basics.ino
@@ -2,11 +2,26 @@
 
 void setup()
 {
+    Serial.begin(250000);
+    while (!Serial);
+
     pinMode(2, OUTPUT);
     pinMode(3, OUTPUT);
 }
 
 void loop()
 {
-    DXL1_write_1mb(0xFF);
+    uint8_t params[255];
+    uint8_t nparams;
+
+    uint16_t res = DXL1_read(2, 2, params, &nparams, 0x00, 2);
+    if (res == DXL__ERRFLAGS_OK) 
+    {
+        Serial.print("Model ");
+        Serial.println(params[0]);
+    }
+    else
+    {
+        DXL1_printErrorMessage(res);
+    }
 }
